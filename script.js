cbeabff5c1fc948bbff6722c5b11d31b78eb5701
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-13 10:51:02 +0500

    Set up hover effect

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ for (let i = 0; i < grid; i++) {
     squareEl.classList.add('square');
     containerEl.appendChild(squareEl);
 }
+
+containerEl.addEventListener('mouseover', (e) => {
+    if (!e.target.classList.contains('square')) return;
+    e.target.classList.add('colored');
+})
