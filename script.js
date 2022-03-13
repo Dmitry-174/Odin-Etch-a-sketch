c46da191639c39a936b60ae1140add67f45c39d1
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-13 12:26:55 +0500

    Add rainbow and black buttons

--- script.js
+++ script.js
@@ -11,16 +11,61 @@ function makeGrid(gridSize) {
     }
 }
 
-makeGrid(16);
+function setNewGridSize() {
+    let gridSize;
+    do {
+        gridSize = prompt('Please input new grid size (maximum 100)')
+    } while (!(gridSize > 0 && gridSize <= 100))
+    return gridSize;
+}
 
-containerEl.addEventListener('mouseover', (e) => {
+function paintBlack(e) {
     if (!e.target.classList.contains('square')) return;
     e.target.classList.add('colored');
+}
+
+function paintRainbow(e) {
+    if (!e.target.classList.contains('square')) return;
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+    e.target.style.backgroundColor = `rgb(${r},${g}, ${b})`;
+}
+
+function toggleButtons() {
+    blackBtn.classList.toggle('active');
+    rainbowBtn.classList.toggle('active');
+}
+
+makeGrid(16);
+
+containerEl.addEventListener('mouseover', paintBlack)
+
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', () => {
+    containerEl.innerHTML = '';
+    gridSize = setNewGridSize();
+    makeGrid(gridSize);
+});
+
+const rainbowBtn = document.querySelector('.rainbow');
+rainbowBtn.addEventListener('click', () => {
+    const coloredElements = document.querySelectorAll('.colored');
+    coloredElements.forEach(element => {
+        element.classList.remove('colored');
+    });
+    containerEl.removeEventListener('mouseover', paintBlack);
+    containerEl.addEventListener('mouseover', paintRainbow)
+    toggleButtons();
 })
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', () => {
-    containerEl.innerHTML = '';
-    grid = prompt('Please input new grid size (maximum 100)');
-    makeGrid(grid);
-});
+const blackBtn = document.querySelector('.black');
+blackBtn.addEventListener('click', () => {
+    const squareElements = document.querySelectorAll('.square');
+    squareElements.forEach(element => {
+        element.style.backgroundColor = '';
+    });
+    containerEl.removeEventListener('mouseover', paintRainbow);
+    containerEl.addEventListener('mouseover', paintBlack);
+    toggleButtons();
+})
